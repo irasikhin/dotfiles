7f0fcf76ee7e9583bc5b6a6e71287a448a633690
irasikhin <i.rasikhin@gmail.com>
2019-12-30 13:17:15 +0300

    add some packages

--- zsh/.config/scripts/init_antigen.sh
+++ zsh/.config/scripts/init_antigen.sh
@@ -11,6 +11,20 @@ antigen bundle pip
 antigen bundle lein
 antigen bundle command-not-found
 antigen bundle autojump
+antigen bundle copyfile
+antigen bundle docker
+antigen bundle encode64
+antigen bundle extract
+antigen bundle gradle
+antigen bundle history
+antigen bundle jira
+antigen bundle lol
+antigen bundle mvn
+antigen bundle sbt
+antigen bundle ssh-agent
+antigen bundle systemd
+antigen bundle wd
+antigen bundle pass
 
 # others
 antigen bundle zsh-users/zsh-syntax-highlighting
