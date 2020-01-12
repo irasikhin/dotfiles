4c2c6c17ac7ef7485e3eea905caabb4880e9a311
irasikhin <i.rasikhin@gmail.com>
2020-01-12 16:05:34 +0300

    install apache pulsar tools

--- zsh/.config/scripts/init_global_env.sh
+++ zsh/.config/scripts/init_global_env.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env zsh
 
+# PATH
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH=$PATH:~/work/app/zulu-jdk12/bin:~/work/app/sbt/bin
 export PATH=$PATH:~/bin
 export PATH=$PATH:~/go/bin
 export PATH=$PATH:~/work/app/mvn/bin
+export PATH=$PATH:~/work/app/pulsar/bin
+
+# OTHER
 export SBT_HOME=~/work/app/sbt
 export JAVA_HOME=~/work/app/zulu-jdk12
 #export GRAALVM_HOME=~/work/app/graalvm
