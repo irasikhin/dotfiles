e024379e1dbf31c3ffce20187d80a601108bd7d9
Ivan Rasikhin <i.rasikhin@gmail.com>
2024-11-01 20:52:03 +0300

    feat: try to run github actions on ubuntu-latest

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-cd ansible && ansible-playbook --connection=local -i env playbook.yml && cd -
+cd ansible && ansible-galaxy role install kewlfft.aur && cd - || exit 1
+cd ansible && ansible-playbook --connection=local -i env playbook.yml && cd - || exit 1
 
 stow i3
 #stow rofi
