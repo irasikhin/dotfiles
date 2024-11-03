1b73bfaa8722f81cb6dafb3f33010fa4d0c225f0
Ivan Rasikhin <i.rasikhin@gmail.com>
2024-11-03 15:17:01 +0300

    feat: refactoring of stow commands

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ cd ansible && ansible-galaxy collection install community.general && cd - || exi
 cd ansible && ansible-galaxy collection install kewlfft.aur && cd - || exit 1
 cd ansible && ansible-playbook --connection=local -i env.yml playbook.yml && cd - || exit
 
-stow i3
-#stow rofi
-#stow tmux
-#stow vim
-stow zsh
+stow -t $HOME/ nvim
+stow -t $HOME/ i3
+stow -t $HOME/ rofi
+stow -t $HOME/ byobu
+stow -t $HOME/ scripts
+stow -t $HOME/ zsh
